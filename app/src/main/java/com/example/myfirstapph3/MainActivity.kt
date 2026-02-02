c5cebb0f302492ac99a6e70de048f984881531a0
TheCodingLeaf <luckygpmaker@gmail.com>
2026-02-02 14:15:46 +0100

    Some BD updates & Clicker Game
    Learnt to reuse Fun where it matters to not clutter my code.
    
    Learn to use Variables in a better way

--- app/src/main/java/com/example/myfirstapph3/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapph3/MainActivity.kt
@@ -15,21 +15,28 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Surface
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.Button
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myfirstapph3.ui.theme.MyFirstAppH3Theme
+import kotlinx.coroutines.delay
 
 /**
  * Main Activity of the Happy Birthday App
@@ -41,31 +48,133 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirstAppH3Theme {
                 var showGreeting by remember { mutableStateOf(false) }
+                var isFromNameInserted by remember { mutableStateOf(false) }
+                var isBDNameInserted by remember { mutableStateOf(false) }
+                var showFromConfirmButton by remember { mutableStateOf(true) }
+                var showBDConfirmButton by remember { mutableStateOf(true) }
+                var fromName by remember { mutableStateOf("") }
+                var toName by remember { mutableStateOf("") }
+
+                //Game Variables
+                var dollarCount by remember { mutableIntStateOf(1000) }
+                var dollarCountPerSecond by remember { mutableIntStateOf(0) }
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Box(modifier = Modifier.fillMaxSize()){
-
                         Image(
                             painter = painterResource(R.drawable.aaaa),
                             contentDescription = null,
                             modifier = Modifier.fillMaxSize(),
                             contentScale = ContentScale.Crop
                         )
-
-                        if(showGreeting){
-                            GreetingImage(
-                                message = stringResource(R.string.happy_birthday_text),
-                                from = stringResource(R.string.happy_birtrhday_from_txt),
-                                modifier = Modifier.padding(8.dp)
+                        ShowDollarAmount(
+                            dollarCount = dollarCount,
+                            dollarCountPerSecond = dollarCountPerSecond,
+                            modifier = Modifier.align(Alignment.TopCenter)
+                        )
+                        Gain1DollarButton(
+                            onClick = { dollarCount++ },
+                            modifier = Modifier.align(Alignment.TopCenter)
+                        )
+                        //Dollars Per Second Shop
+                        if(dollarCount >= 10){
+                            AddDollarsASecButton(
+                                onClick = {
+                                    dollarCount -= 10
+                                    dollarCountPerSecond++
+                                },
+                                value = "+1$/s Cost: 10$",
+                                modifier = Modifier
+                                    .align(Alignment.TopCenter)
+                                    .padding(top = 150.dp)
                             )
                         }
-                        ShowBDtextButton(
-                            onClick = { showGreeting = !showGreeting },
-                            isOn = showGreeting,
-                            modifier = Modifier.align(Alignment.BottomCenter)
-                        )
+                        if(dollarCount >= 100){
+                            AddDollarsASecButton(
+                                onClick = {
+                                    dollarCount -= 100
+                                    dollarCountPerSecond += 10
+                                },
+                                value = "+10$/s Cost: 100$",
+                                modifier = Modifier
+                                    .align(Alignment.TopCenter)
+                                    .padding(top = 195.dp)
+                            )
+                        }
+                        if(dollarCount >= 1000){
+                            AddDollarsASecButton(
+                                onClick = {
+                                    dollarCount -= 1000
+                                    dollarCountPerSecond += 100
+                                },
+                                value = "+100$/s  Cost: 1000$",
+                                modifier = Modifier
+                                    .align(Alignment.TopCenter)
+                                    .padding(top = 240.dp)
+                            )
+                        }
+                        //Dollars Per Second Calculations
+                        LaunchedEffect(Unit) {
+                            while(true) {
+                                if(dollarCountPerSecond > 0) {
+                                    dollarCount += dollarCountPerSecond
+                                }
+                                delay(1000L) // 1 second
+                            }
+                        }
+
+
+
+//                        if(showGreeting){
+//                            GreetingImage(
+//                                message = "Happy Birthday - $toName",
+//                                from = "From - $fromName",
+//                                modifier = Modifier.padding(8.dp)
+//                            )
+//                        }
+//                        if(!showFromConfirmButton){
+//                            ShowBirthDayTextButton(
+//                                onClick = { showGreeting = !showGreeting },
+//                                isOn = showGreeting,
+//                                modifier = Modifier.align(Alignment.BottomCenter)
+//                            )
+//                        }
+//                        if(showFromConfirmButton){
+//                            AppButton(
+//                                onClick = { isFromNameInserted = true} ,
+//                                value = "Confirm",
+//                                modifier = Modifier.align(Alignment.BottomCenter)
+//                            )
+//                            AppTextField(
+//                                value = fromName,
+//                                onValueChange = {value -> fromName = value },
+//                                label = "Enter Recipient's Name",
+//                                modifier = Modifier.align(Alignment.Center)
+//                            )
+//                        }
+//                        if(showBDConfirmButton){
+//                            AppButton(
+//                                onClick = { isBDNameInserted = true} ,
+//                                value = "Confirm",
+//                                modifier = Modifier.align(Alignment.BottomCenter)
+//                            )
+//                            AppTextField(
+//                                value = toName,
+//                                onValueChange = {value -> toName = value },
+//                                label = "Enter Your Name",
+//                                modifier = Modifier.align(Alignment.Center)
+//                            )
+//                        }
+//                        //This seems like it could be done better later on
+//                        if(isFromNameInserted){
+//                            showFromConfirmButton = false
+//                        }
+//                        if(isBDNameInserted){
+//                            showBDConfirmButton = false
+//                        }
                     }
                 }
             }
@@ -117,7 +226,6 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
         )
     }
 }
-
 @Preview (showBackground = true)
 @Composable
 fun BirthdayCardPreview(){
@@ -130,9 +238,8 @@ fun BirthdayCardPreview(){
         }
     }
 }
-
 @Composable
-fun ShowBDtextButton(
+fun ShowBirthDayTextButton(
     onClick: () -> Unit,
     isOn: Boolean,
     modifier: Modifier = Modifier
@@ -140,10 +247,99 @@ fun ShowBDtextButton(
     Button(
         onClick = onClick,
         modifier = modifier.offset(y = (-50).dp)
+
     ) {
         Text(
-            if (isOn) "Hide Greeting" else "Show Greeting",
+            if (isOn) "Close" else "Open",
             color = Color.White
+        )
+        Image(
+            painter = painterResource(R.drawable.gift),
+            modifier = Modifier.offset(x = 10.dp),
+            contentDescription = null,
+        )
+    }
+}
+//Reusable Button
+@Composable
+fun AppButton(
+    onClick: () -> Unit,
+    value: String,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.padding(bottom = 370.dp)
+    ){
+        Text(
+            value
+        )
+    }
+}
+//Reusable TextField
+@Composable
+fun AppTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier
+){
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        modifier = modifier
+    )
+}
+//Clicker Game
+@Composable
+fun ShowDollarAmount(
+    dollarCount: Int,
+    dollarCountPerSecond: Int,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = buildAnnotatedString {
+            append("You have ")
+            withStyle(SpanStyle(color = Color.Green)) {
+                append("$dollarCount")
+            }
+            append(" dollars")
+            append(" ")
+            withStyle(SpanStyle(color = Color.Green)) {
+                append("+$dollarCountPerSecond")
+            }
+        },
+        color = Color.White,
+        modifier = modifier.padding(top = 70.dp)
+    )
+}
+@Composable
+fun Gain1DollarButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.padding(top = 100.dp)
+    ){
+        Text(
+            "+1$"
+        )
+    }
+}
+@Composable
+fun AddDollarsASecButton(
+    onClick: () -> Unit,
+    value: String,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+    ){
+        Text(
+            text = value
         )
     }
 }
